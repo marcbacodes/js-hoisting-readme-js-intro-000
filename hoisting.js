b84583a752431905e3bf3349e350b1d19a5dc314
Marc BA <marc.bisbalarias@dowjones.com>
2019-10-08 07:43:30 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,9 +1,9 @@
 function callMe() {
+var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-var lyric = "maybe";
 return lyric;
 }
 
@@ -12,7 +12,7 @@ function crazy() {
   // fix the code in here:
   //thisIsCrazy();
 
-  function thisIsCrazy(){
+  var thisIsCrazy=function(){
     console.log("hey!!!")
 
   thisIsCrazy();
@@ -27,7 +27,7 @@ function sayMy() {
 var name = "Kristin";
 console.log(name);
 
-sayMy();
+return sayMy();
 
   }
 }
